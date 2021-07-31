0189f102b2ea4e11b82bc18cafdcb14288f1ff4e
Daniele Cambi <dancam.dev@gmail.com>
2021-07-31 16:28:29 +0200

    Initial commit

---
+++ lib/appandup_lint.dart
@@ -0,0 +1,14 @@
+/// The appandup_lint package doesn't ship any dart source code.
+///
+/// To enable `appandup_lint`,
+/// 1. Add it to your dev_dependencies
+/// ```yaml
+/// dev_dependencies:
+///   appandup_lint: ^1.0.0
+/// ```
+///
+/// 2. Include the rules into your `analysis_options.yaml`
+/// ```yaml
+/// include: package:appandup_lint/analysis_options.yaml
+/// ```
+library appandup_lint;
